30911979ffabfbee650d80e405bb48713ab60cc8
Monica Sumie Hieda Dias <msumiedias@gmail.com>
2022-03-18 19:54:09 -0300

    att

--- index.js
+++ index.js
@@ -2,37 +2,45 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-const port = 3000;
-let message = '';
+const port = process.env.PORT || 3000;
+
+let message = "";
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", function (req, res) {
-    const devList = ["Backend", "Frontend", "Fullstack", "DEvSEC"];
-    setTimeout(() => {
-        message = "";
-      }, 3000);
-  res.render("index", { titulo: "TESTE aula", nome: "thiago", devList: devList, message });
+  const devList = ["Backend", "Frontend", "Fullstack", "DEvSEC"];
+  setTimeout(() => {
+    message = "";
+  }, 3000);
+  res.render("index", {
+    titulo: "TESTE aula",
+    nome: "thiago",
+    devList: devList,
+    message,
+  });
 });
 
 app.post("/", function (req, res) {
-    res.send("Hello World via post");
+  res.send("Hello World via post");
 });
 
 app.get("/pagina", function (req, res) {
-    res.send("pagina 2");
+  res.send("pagina 2");
 });
 
 app.post("/subscription", (req, res) => {
-    const { nome, email } = req.body;
-    if(nome=="leo"){
-        message = `Usuário ${nome} já cadastrado!`
-    }else{
-        message = `Parabéns ${nome}, sua inscrição foi realizada com sucesso! Um e-mail foi enviado para: ${email}`;}
-    res.redirect("/");
-  });
+  const { nome, email } = req.body;
+  if (nome == "leo") {
+    message = `Usuário ${nome} já cadastrado!`;
+  } else {
+    message = `Parabéns ${nome}, sua inscrição foi realizada com sucesso! Um e-mail foi enviado para: ${email}`;
+  }
+  res.redirect("/");
+});
 
-
-app.listen(port, () => console.log(`Servidor rodando em http://localhost:${port}`));
+app.listen(port, () =>
+  console.log(`Servidor rodando em http://localhost:${port}`)
+);
